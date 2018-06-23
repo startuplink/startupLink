ad5b15f5381ebf5b3a8780bd4378999c5f95ade6
dmytro <dy.lyahov@gmail.com>
2018-06-23 15:48:19 +0300

    Implement add/remove/save values in storage

--- popup/startupLinkPopup.js
+++ popup/startupLinkPopup.js
@@ -1,3 +1,4 @@
+var $body = $(".body");
 
 initialize();
 initEvents();
@@ -7,7 +8,7 @@ function initialize() {
     storage.then((result) => {
         var keys = Object.keys(result);
         for (let key of keys) {
-            loadLink(key, result[key]);
+            addLinkForm(result[key]);
         }
     });
 }
@@ -15,7 +16,7 @@ function initialize() {
 function loadLink(key, body) {
     $('.bd-main input[type="text"]').val(body.reference);
     $('.bd-main input[type="checkbox"]').prop('checked', body.pinned);
-    $('.bd-main .form-group').attr('data-item-id', key);
+    $('.bd-main .form-group').attr('data-link-id', key);
 }
 
 function setLink(e) {
@@ -23,7 +24,7 @@ function setLink(e) {
     var $baseElement = $event.closest('.form-group');
 
     var link = {};
-    link.id = $baseElement.data('item-id');
+    link.id = $baseElement.data('link-id');
     link.reference = $baseElement.find('input.form-control[type="text"]').val();
     link.pinned = $baseElement.find('input.form-control[type="checkbox"]').prop('checked');
     
@@ -31,24 +32,43 @@ function setLink(e) {
 }
 
 
-function addLinkForm() {
+function addLinkForm(data) {
     var linkForm = `    
         <div class="form-group">
             <label>Link</label>
             <input type="text" class="form-control">
-            <input type="checkbox">
+            <input type="checkbox" class="form-control">
             <input type="button" class="remove-link" value="Remove">
         </div>`;
 
+
     var $linkForm = $(linkForm);
     var $linkContainer = $('.link-container');
-    $linkForm.attr("data-item-id", "link" + $linkContainer.children('.form-group').length);
+    $linkForm.attr("data-link-id", "link" + $linkContainer.children('.form-group').length);
     
     $linkContainer.append($linkForm);
+    $linkForm.find('.remove-link').click(removeLinkForm);
+    
+    var $linkInput = $linkForm.find('input[type="text"]');
+    var $linkPinned = $linkForm.find('input[type="checkbox"]');
+
+    if (data) {
+        $linkForm.attr('data-link-id', data.id);
+        $linkPinned[0].checked = data.pinned;
+        $linkInput.val(data.reference);
+    }
+
+    $linkInput.keyup(setLink);
+    $linkPinned.change(setLink);
+}
+
+function removeLinkForm(event) {
+    var $target = $(event.target);
+    var $formElement = $target.closest('.form-group');
+    browser.storage.local.remove([$formElement.data('link-id')]);
+    $target.closest('.form-group').remove();
 }
 
 function initEvents() {
-    $('.form-group input[type="text"]').keyup(setLink);
-    $('.form-group input[type="checkbox"]').change(setLink);
     $('.bd-main .add-link').click(addLinkForm);
 }
